61906e4c5aa8c119f2a50356666393b00b54ff58
Zakarias Bergcrona <zb222cw@student.lnu.se>
2024-02-07 17:59:36 +0100

    Adds setupTestServer

---
+++ src/utils/setupTestServer.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+export function setupTestServer() {
+    const app = express();
+    app.use(express.json({ limit: '50mb' }));
+  
+    app.post('/test', (req, res) => {
+      res.json(req.body);
+    });
+  
+    let server = app.listen(3000, () => console.log(`Test server running on port 3000`));
+  
+    return {
+      close: () => server.close(),
+    };
+  }
